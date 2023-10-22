3af7c4465a0be6dfcc79abdc8f2bdb0d12687b1f
Matvey Sergeev <enhisir@yandex.ru>
2023-10-22 22:52:37 +0300

    выполнил кр (lvl 3)

--- activity.sql
+++ activity.sql
@@ -1,1 +1,38 @@
----
+with recursive cte as (
+	select 1 as level, act.parentid, crt.areaid, act.name as path
+	from activity as act
+		join point as pt
+            on act.activityid = pt.pointid
+		join 
+		(
+			select act.activityid, crt.areaid
+			from activity as act
+				join contract as crt
+					on act.activityid = crt.contractid
+		) as crt
+			on act.parentid = crt.activityid
+	where activitytypeid = 5
+	
+	union
+	
+	select cte.level + 1 as level, act.parentid, cte.areaid,
+		case
+			when level = 2 
+				then (select name from area where cte.areaid = area.areaid) || ' -> '
+					 || act.name || ' -> '
+					 || (select act2.name
+						 from activity as act2
+						 join point as pt
+						 	on act2.activityid = pt.pointid
+						 where act2.parentid = act.activityid
+						) || ' -> '
+					 || cte.path
+			else act.name || ' -> ' || cte.path
+		end
+	from activity as act
+		join cte
+			on act.activityid = cte.parentid
+)
+select path
+from cte
+where level = 5;
